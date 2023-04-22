0e601bf5d356f2c6715ab077db3b488e6baf006c
923404422 <48235127+923404422@users.noreply.github.com>
2023-04-22 21:45:45 +0800

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -10,3 +10,9 @@ if [[ $OWRT_URL == *"immortalwrt"* ]] ; then
   echo "CONFIG_PACKAGE_luci=y" >> .config
   echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
 fi
+#根据源码来修改
+if [[ $OWRT_URL == *"lede"* ]] ; then
+  #增加luci界面
+  echo "CONFIG_PACKAGE_luci=y" >> .config
+  echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
+fi
